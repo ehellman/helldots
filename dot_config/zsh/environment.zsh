cf6fc67c025cea9b5cba02b692b158b80f3bca6b
Erik Hellman <iam@erikhellman.com>
2024-10-27 14:21:01 +0100

    Chezmoi init

---
+++ dot_config/zsh/environment.zsh
@@ -0,0 +1,5 @@
+# XDG Base Directory Specification
+# http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
