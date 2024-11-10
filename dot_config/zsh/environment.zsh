64dce19161e75a6e7b66b705e94e3869f68efe70
Erik Hellman <iam@erikhellman.com>
2024-11-10 10:55:59 +0100

    Add wayland env variables for firefox

--- dot_config/zsh/environment.zsh
+++ dot_config/zsh/environment.zsh
@@ -3,3 +3,10 @@
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
+
+
+if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
+  # enable wayland env variables for firefox
+  export MOZ_DBUS_REMOTE=1
+  export MOZ_ENABLE_WAYLAND=1
+fi
