65ed59fea5d50a19ba90a35f20abf23338770950
Erik Hellman <iam@erikhellman.com>
2025-01-19 03:13:22 +0100

    Add yazi alias

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,1 +1,10 @@
 alias jctl="journalctl -p 3 -xb"
+
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    builtin cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
+}
