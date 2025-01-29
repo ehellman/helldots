69c4fdec22fa88570d60c974bb1925ba65e0d49c
Erik Hellman <iam@erikhellman.com>
2025-01-29 18:01:24 +0100

    chore(yazi): register exit dir with zoxide

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -4,6 +4,7 @@ alias ls='eza --color=auto --icons --group-directories-first'
 alias l='ls -lhF'
 alias la='ls -lhAF'
 alias tree='ls --tree'
+alias cd='z'
 
 alias cat='bat --wrap character'
 alias ping='gping'
@@ -15,6 +16,7 @@ function y() {
 	yazi "$@" --cwd-file="$tmp"
 	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
 		builtin cd -- "$cwd"
+		zoxide add "$cwd"
 	fi
 	rm -f -- "$tmp"
 }
