0869d30f972d8164d4b96fe7304c6ea8da97451f
Erik Hellman <iam@erikhellman.com>
2025-02-20 12:43:27 +0100

    chore(zsh): updates to zsh environment

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -10,6 +10,17 @@ alias cat='bat --wrap character'
 alias ping='gping'
 alias yay='paru'
 
+alias t='tmux'
+alias ta='tmux attach'
+alias tl='tmux list-sessions'
+alias tk='tmux kill-session -t'
+
+function n() {
+  APPNAME=$(gum choose --header "Which nvim configuration would you like to use?" "hellman" "lazyvim" "lazydev")
+  NVIM_APPNAME=nvim-$APPNAME nvim "$@"
+}
+
+alias nvim='NVIM_APPNAME=nvim-hellman nvim'
 
 function y() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
--- dot_config/zsh/load-plugins.zsh
+++ dot_config/zsh/load-plugins.zsh
@@ -1,3 +1,4 @@
 [ -f $ZSH_PLUGIN_DIR/zsh-defer/zsh-defer.plugin.zsh ] && source $ZSH_PLUGIN_DIR/zsh-defer/zsh-defer.plugin.zsh;
 [ -f $ZSH_PLUGIN_DIR/zsh-autosuggestions.zsh ] && zsh-defer source $ZSH_PLUGIN_DIR/zsh-autosuggestions.zsh;
 [ -f $ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && zsh-defer source $ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh;
+[ -f $ZSH_PLUGIN_DIR/zsh-system-clipboard/zsh-system-clipboard.zsh ] && zsh-defer source $ZSH_PLUGIN_DIR/zsh-system-clipboard/zsh-system-clipboard.zsh;
