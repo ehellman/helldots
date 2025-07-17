1874886ec6a130303152a18767739c22c5948cf7
Erik Hellman <iam@erikhellman.com>
2025-07-17 08:45:43 +0200

    chore(path): add cargo and pnpm to path

--- dot_config/zsh/path.zsh
+++ dot_config/zsh/path.zsh
@@ -1,4 +1,6 @@
 LOCAL_BIN="$HOME/.local/bin"
 MISE_SHIMS="$HOME/.local/share/mise/shims"
+CARGO_BIN="$HOME/.local/share/cargo/bin"
+PNPM_HOME="$HOME/.local/share/pnpm"
 
-export PATH="$LOCAL_BIN:$MISE_SHIMS:$PATH"
+export PATH="$LOCAL_BIN:$MISE_SHIMS:$PNPM_HOME:$CARGO_BIN:$PATH"
