56341525735edae912fdde0ce614bf39004ab9a5
Erik Hellman <iam@erikhellman.com>
2026-02-09 16:53:49 +0100

    chore(zsh): move pyenv to proper files

--- dot_config/zsh/path.zsh
+++ dot_config/zsh/path.zsh
@@ -2,5 +2,7 @@ LOCAL_BIN="$HOME/.local/bin"
 MISE_SHIMS="$HOME/.local/share/mise/shims"
 CARGO_BIN="$HOME/.local/share/cargo/bin"
 PNPM_HOME="$HOME/.local/share/pnpm"
+OPENCODE_BIN="$HOME/.opencode/bin"
+PYENV_BIN="$PYENV_ROOT/bin"
 
-export PATH="$LOCAL_BIN:$MISE_SHIMS:$PNPM_HOME:$CARGO_BIN:$PATH"
+export PATH="$LOCAL_BIN:$MISE_SHIMS:$PNPM_HOME:$CARGO_BIN:$OPENCODE_BIN:$PYENV_BIN:$PATH"
